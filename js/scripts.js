5616c2149128c0f6ec328664bee609840d1c8e24
LeiKD <dkomen180@gmail.com>
2020-02-23 00:27:26 +0300

    Add js function

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
+function enter() {
+  var name= document.getElementById("name").value
+  var email= document.getElementById("email").value
+  var text= document.getElementById("text").value
+  if (name==="" || email==="" || text==="") {
+    alert("Input the required data!")
+  } else {
+    alert(name + "," + "we have received your message. Thank you for reaching out to us.")
+  }
+
+
+
 $(document).ready(function() {
   $(".design").click( function() {
     $("#design-showing").toggle()
