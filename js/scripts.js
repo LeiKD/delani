60360178b09b42f259c9f55a9b7e8a18aba41bfd
LeiKD <dkomen180@gmail.com>
2020-02-22 19:22:30 +0300

    Add ids

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,14 @@
 $(document).ready(function() {
-  $("img#design").click( function() {
+  $("#design").click( function() {
     $("#design-showing").toggle()
-  })
-  $("p").click( function() {
     $("#design-hidden").toggle()
   })
-  $("img#dev").click(function() {
+  $("#dev").click(function() {
     $("#dev-showing").toggle()
-  })
-  $("p").click(function(){
     $("#dev-hidden").toggle()
   })
-  $("img#product").click(function() {
+  $("#product").click(function() {
     $("#product-showing").toggle()
-  })
-  $("p").click( function() {
     $("#product-hidden").toggle()
   })
 })
